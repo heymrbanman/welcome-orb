057cdc8dd42bbe5572e3ed1f99f4cf0af1af6abb
John Murret <john.murret@circleci.com>
2019-11-21 17:51:14 -0700

    [WIP] CIRCLE-21394 - Welcome Orb - Improving the Config (#2)
    * CIRCLE-21394 - improve welcome-orb
    
    * CIRCLE-21394 - improve welcome-orb
    
    * updating config

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM circleci/node:10.15.3
+MAINTAINER CircleCI sayhi@circleci.com
+
+ADD bin/* /usr/local/bin/
---
+++ bin/using_the_cli.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+source setup_term_styles.sh
+
+echo -e $SEPARATOR
+echo -e "${HEADER_TEXT}Using the CLI${NORMAL_TEXT}
+$SEPARATOR
+
+${SECTION_TEXT}Getting Started${NORMAL_TEXT}
+
+${UNDERLINED_TEXT}https://circleci.com/docs/2.0/local-cli-getting-started/ ${NORMAL_TEXT}
+
+This section offers help on:
+- Working with a sample repository
+- Download and Setup
+- Validate Your First Config
+- Testing a Job before Pushing to VCS
+
+${SECTION_TEXT}Using the CLI${NORMAL_TEXT}
+
+${UNDERLINED_TEXT}https://circleci.com/docs/2.0/local-cli/ ${NORMAL_TEXT}
+
+This section offers help on:
+- Validating a CircleCI config
+- Packing A Config
+- Processing A Config
+- Running a Job in a Container on Your Machine
+"
